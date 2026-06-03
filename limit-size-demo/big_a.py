2e467f2f53de55395d36f31baa2a8ca0061db9f2
ye12121 <1010566335@qq.com>
2026-06-03 22:23:51 +0800

    demo: big_a part 1/2 (750 lines)

---
+++ limit-size-demo/big_a.py
@@ -0,0 +1,750 @@
+# auto-generated demo file: big_a.py (1500 lines)
+def func_1(x):
+    # step 1
+    return x + 1
+
+def func_2(x):
+    # step 2
+    return x + 2
+
+def func_3(x):
+    # step 3
+    return x + 3
+
+def func_4(x):
+    # step 4
+    return x + 4
+
+def func_5(x):
+    # step 5
+    return x + 5
+
+def func_6(x):
+    # step 6
+    return x + 6
+
+def func_7(x):
+    # step 7
+    return x + 7
+
+def func_8(x):
+    # step 8
+    return x + 8
+
+def func_9(x):
+    # step 9
+    return x + 9
+
+def func_10(x):
+    # step 10
+    return x + 10
+
+def func_11(x):
+    # step 11
+    return x + 11
+
+def func_12(x):
+    # step 12
+    return x + 12
+
+def func_13(x):
+    # step 13
+    return x + 13
+
+def func_14(x):
+    # step 14
+    return x + 14
+
+def func_15(x):
+    # step 15
+    return x + 15
+
+def func_16(x):
+    # step 16
+    return x + 16
+
+def func_17(x):
+    # step 17
+    return x + 17
+
+def func_18(x):
+    # step 18
+    return x + 18
+
+def func_19(x):
+    # step 19
+    return x + 19
+
+def func_20(x):
+    # step 20
+    return x + 20
+
+def func_21(x):
+    # step 21
+    return x + 21
+
+def func_22(x):
+    # step 22
+    return x + 22
+
+def func_23(x):
+    # step 23
+    return x + 23
+
+def func_24(x):
+    # step 24
+    return x + 24
+
+def func_25(x):
+    # step 25
+    return x + 25
+
+def func_26(x):
+    # step 26
+    return x + 26
+
+def func_27(x):
+    # step 27
+    return x + 27
+
+def func_28(x):
+    # step 28
+    return x + 28
+
+def func_29(x):
+    # step 29
+    return x + 29
+
+def func_30(x):
+    # step 30
+    return x + 30
+
+def func_31(x):
+    # step 31
+    return x + 31
+
+def func_32(x):
+    # step 32
+    return x + 32
+
+def func_33(x):
+    # step 33
+    return x + 33
+
+def func_34(x):
+    # step 34
+    return x + 34
+
+def func_35(x):
+    # step 35
+    return x + 35
+
+def func_36(x):
+    # step 36
+    return x + 36
+
+def func_37(x):
+    # step 37
+    return x + 37
+
+def func_38(x):
+    # step 38
+    return x + 38
+
+def func_39(x):
+    # step 39
+    return x + 39
+
+def func_40(x):
+    # step 40
+    return x + 40
+
+def func_41(x):
+    # step 41
+    return x + 41
+
+def func_42(x):
+    # step 42
+    return x + 42
+
+def func_43(x):
+    # step 43
+    return x + 43
+
+def func_44(x):
+    # step 44
+    return x + 44
+
+def func_45(x):
+    # step 45
+    return x + 45
+
+def func_46(x):
+    # step 46
+    return x + 46
+
+def func_47(x):
+    # step 47
+    return x + 47
+
+def func_48(x):
+    # step 48
+    return x + 48
+
+def func_49(x):
+    # step 49
+    return x + 49
+
+def func_50(x):
+    # step 50
+    return x + 50
+
+def func_51(x):
+    # step 51
+    return x + 51
+
+def func_52(x):
+    # step 52
+    return x + 52
+
+def func_53(x):
+    # step 53
+    return x + 53
+
+def func_54(x):
+    # step 54
+    return x + 54
+
+def func_55(x):
+    # step 55
+    return x + 55
+
+def func_56(x):
+    # step 56
+    return x + 56
+
+def func_57(x):
+    # step 57
+    return x + 57
+
+def func_58(x):
+    # step 58
+    return x + 58
+
+def func_59(x):
+    # step 59
+    return x + 59
+
+def func_60(x):
+    # step 60
+    return x + 60
+
+def func_61(x):
+    # step 61
+    return x + 61
+
+def func_62(x):
+    # step 62
+    return x + 62
+
+def func_63(x):
+    # step 63
+    return x + 63
+
+def func_64(x):
+    # step 64
+    return x + 64
+
+def func_65(x):
+    # step 65
+    return x + 65
+
+def func_66(x):
+    # step 66
+    return x + 66
+
+def func_67(x):
+    # step 67
+    return x + 67
+
+def func_68(x):
+    # step 68
+    return x + 68
+
+def func_69(x):
+    # step 69
+    return x + 69
+
+def func_70(x):
+    # step 70
+    return x + 70
+
+def func_71(x):
+    # step 71
+    return x + 71
+
+def func_72(x):
+    # step 72
+    return x + 72
+
+def func_73(x):
+    # step 73
+    return x + 73
+
+def func_74(x):
+    # step 74
+    return x + 74
+
+def func_75(x):
+    # step 75
+    return x + 75
+
+def func_76(x):
+    # step 76
+    return x + 76
+
+def func_77(x):
+    # step 77
+    return x + 77
+
+def func_78(x):
+    # step 78
+    return x + 78
+
+def func_79(x):
+    # step 79
+    return x + 79
+
+def func_80(x):
+    # step 80
+    return x + 80
+
+def func_81(x):
+    # step 81
+    return x + 81
+
+def func_82(x):
+    # step 82
+    return x + 82
+
+def func_83(x):
+    # step 83
+    return x + 83
+
+def func_84(x):
+    # step 84
+    return x + 84
+
+def func_85(x):
+    # step 85
+    return x + 85
+
+def func_86(x):
+    # step 86
+    return x + 86
+
+def func_87(x):
+    # step 87
+    return x + 87
+
+def func_88(x):
+    # step 88
+    return x + 88
+
+def func_89(x):
+    # step 89
+    return x + 89
+
+def func_90(x):
+    # step 90
+    return x + 90
+
+def func_91(x):
+    # step 91
+    return x + 91
+
+def func_92(x):
+    # step 92
+    return x + 92
+
+def func_93(x):
+    # step 93
+    return x + 93
+
+def func_94(x):
+    # step 94
+    return x + 94
+
+def func_95(x):
+    # step 95
+    return x + 95
+
+def func_96(x):
+    # step 96
+    return x + 96
+
+def func_97(x):
+    # step 97
+    return x + 97
+
+def func_98(x):
+    # step 98
+    return x + 98
+
+def func_99(x):
+    # step 99
+    return x + 99
+
+def func_100(x):
+    # step 100
+    return x + 100
+
+def func_101(x):
+    # step 101
+    return x + 101
+
+def func_102(x):
+    # step 102
+    return x + 102
+
+def func_103(x):
+    # step 103
+    return x + 103
+
+def func_104(x):
+    # step 104
+    return x + 104
+
+def func_105(x):
+    # step 105
+    return x + 105
+
+def func_106(x):
+    # step 106
+    return x + 106
+
+def func_107(x):
+    # step 107
+    return x + 107
+
+def func_108(x):
+    # step 108
+    return x + 108
+
+def func_109(x):
+    # step 109
+    return x + 109
+
+def func_110(x):
+    # step 110
+    return x + 110
+
+def func_111(x):
+    # step 111
+    return x + 111
+
+def func_112(x):
+    # step 112
+    return x + 112
+
+def func_113(x):
+    # step 113
+    return x + 113
+
+def func_114(x):
+    # step 114
+    return x + 114
+
+def func_115(x):
+    # step 115
+    return x + 115
+
+def func_116(x):
+    # step 116
+    return x + 116
+
+def func_117(x):
+    # step 117
+    return x + 117
+
+def func_118(x):
+    # step 118
+    return x + 118
+
+def func_119(x):
+    # step 119
+    return x + 119
+
+def func_120(x):
+    # step 120
+    return x + 120
+
+def func_121(x):
+    # step 121
+    return x + 121
+
+def func_122(x):
+    # step 122
+    return x + 122
+
+def func_123(x):
+    # step 123
+    return x + 123
+
+def func_124(x):
+    # step 124
+    return x + 124
+
+def func_125(x):
+    # step 125
+    return x + 125
+
+def func_126(x):
+    # step 126
+    return x + 126
+
+def func_127(x):
+    # step 127
+    return x + 127
+
+def func_128(x):
+    # step 128
+    return x + 128
+
+def func_129(x):
+    # step 129
+    return x + 129
+
+def func_130(x):
+    # step 130
+    return x + 130
+
+def func_131(x):
+    # step 131
+    return x + 131
+
+def func_132(x):
+    # step 132
+    return x + 132
+
+def func_133(x):
+    # step 133
+    return x + 133
+
+def func_134(x):
+    # step 134
+    return x + 134
+
+def func_135(x):
+    # step 135
+    return x + 135
+
+def func_136(x):
+    # step 136
+    return x + 136
+
+def func_137(x):
+    # step 137
+    return x + 137
+
+def func_138(x):
+    # step 138
+    return x + 138
+
+def func_139(x):
+    # step 139
+    return x + 139
+
+def func_140(x):
+    # step 140
+    return x + 140
+
+def func_141(x):
+    # step 141
+    return x + 141
+
+def func_142(x):
+    # step 142
+    return x + 142
+
+def func_143(x):
+    # step 143
+    return x + 143
+
+def func_144(x):
+    # step 144
+    return x + 144
+
+def func_145(x):
+    # step 145
+    return x + 145
+
+def func_146(x):
+    # step 146
+    return x + 146
+
+def func_147(x):
+    # step 147
+    return x + 147
+
+def func_148(x):
+    # step 148
+    return x + 148
+
+def func_149(x):
+    # step 149
+    return x + 149
+
+def func_150(x):
+    # step 150
+    return x + 150
+
+def func_151(x):
+    # step 151
+    return x + 151
+
+def func_152(x):
+    # step 152
+    return x + 152
+
+def func_153(x):
+    # step 153
+    return x + 153
+
+def func_154(x):
+    # step 154
+    return x + 154
+
+def func_155(x):
+    # step 155
+    return x + 155
+
+def func_156(x):
+    # step 156
+    return x + 156
+
+def func_157(x):
+    # step 157
+    return x + 157
+
+def func_158(x):
+    # step 158
+    return x + 158
+
+def func_159(x):
+    # step 159
+    return x + 159
+
+def func_160(x):
+    # step 160
+    return x + 160
+
+def func_161(x):
+    # step 161
+    return x + 161
+
+def func_162(x):
+    # step 162
+    return x + 162
+
+def func_163(x):
+    # step 163
+    return x + 163
+
+def func_164(x):
+    # step 164
+    return x + 164
+
+def func_165(x):
+    # step 165
+    return x + 165
+
+def func_166(x):
+    # step 166
+    return x + 166
+
+def func_167(x):
+    # step 167
+    return x + 167
+
+def func_168(x):
+    # step 168
+    return x + 168
+
+def func_169(x):
+    # step 169
+    return x + 169
+
+def func_170(x):
+    # step 170
+    return x + 170
+
+def func_171(x):
+    # step 171
+    return x + 171
+
+def func_172(x):
+    # step 172
+    return x + 172
+
+def func_173(x):
+    # step 173
+    return x + 173
+
+def func_174(x):
+    # step 174
+    return x + 174
+
+def func_175(x):
+    # step 175
+    return x + 175
+
+def func_176(x):
+    # step 176
+    return x + 176
+
+def func_177(x):
+    # step 177
+    return x + 177
+
+def func_178(x):
+    # step 178
+    return x + 178
+
+def func_179(x):
+    # step 179
+    return x + 179
+
+def func_180(x):
+    # step 180
+    return x + 180
+
+def func_181(x):
+    # step 181
+    return x + 181
+
+def func_182(x):
+    # step 182
+    return x + 182
+
+def func_183(x):
+    # step 183
+    return x + 183
+
+def func_184(x):
+    # step 184
+    return x + 184
+
+def func_185(x):
+    # step 185
+    return x + 185
+
+def func_186(x):
+    # step 186
+    return x + 186
+
+def func_187(x):
+    # step 187
+    return x + 187
+
+def func_188(x):
